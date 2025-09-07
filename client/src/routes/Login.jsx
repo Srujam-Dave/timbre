fccb8c0e850a3ae8362351e605dc1c437f62d25a
Srujam Dave <srdave@ucsd.edu>
2025-09-07 14:14:10 -0700

    Redesign login page

--- client/src/routes/Login.jsx
+++ client/src/routes/Login.jsx
@@ -20,7 +20,7 @@ const Login = () => {
             <a href={loginUrl}>
                 <div id={styles.login_button}>
                     <p>Log in with </p>
-                    <img src="../src/assets/SpotifyLogo.svg" id={styles.logo}/>
+                    <img src="../src/assets/SpotifyLogo.svg"/>
                 </div>
             </a>
         );
@@ -28,24 +28,10 @@ const Login = () => {
 
     return (
         <div className={styles.main}>
-            <section id={styles.header}>
-                <img src='../src/assets/Untitled.png' />
-                <h1>TIMBRE - VIEW YOUR MUSIC</h1>
-                <p>Celebrate your top artists and rediscover forgotten favorites.</p>
-            </section>
-            <div className={styles.content}>
-                <section id={styles.top}>
-                    <h1>COMPLETELY FREE</h1>
-                    <p>Most sites hide important listening habits behind a paywall. 
-                        Not here - view all your most important information at the click of a button. </p>
-                    <p>Log in and start viewing!</p>
-                </section>
-                <section id={styles.bottom}>
-                    <h1>SEAMLESSLY INTEGRATED</h1>
-                    <p>Just log into Timbre with your Spotify account and see your listening habits!</p>
-                    <LoginButton />
-                </section>
-            </div>
+            <img src='../src/assets/Untitled.png' id={styles.timbre}/>
+                <h1 id={styles.head}>TIMBRE - VIEW YOUR MUSIC</h1>
+                <p id={styles.paragraph}>Just log into Timbre with your Spotify account and see your listening habits!</p>
+                <LoginButton id={styles.fadesin}/>
         </div>
     );
 }
